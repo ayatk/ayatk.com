72749a4f7452013f52f10a04e7371d10f4dd965f
Aya Tokikaze <git@ayatk.com>
2017-06-04 22:23:37 +0900

    Fix e2e code error

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class Ayatk.ComPage {
+export class AyatkPage {
   navigateTo() {
     return browser.get('/');
   }
