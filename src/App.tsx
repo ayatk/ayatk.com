8e24c301bec4515c76f24f1b6d0cd54a20f7cd54
ayatk <git@ayatk.com>
2020-04-12 11:55:45 +0900

    styled-componentを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import * as React from "react";
 import Links from "./components/Links";
+import styled from "styled-components";
+
+const RootWrapper = styled.div`
+  font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", "Yu Gothic",
+    YuGothic, "ヒラギノ角ゴ", sans-serif;
+  display: flex;
+`;
+
+const Wrapper = styled.div`
+  width: 80rem;
+  margin: 7.5rem auto;
+`;
 
 const App = () => {
   const title = "(´・ω・｀)やあ";
   const message = "以下のリンクのどれかを選ぶとええぞ…";
 
   return (
-    <div className="wrapper">
-      <div className="container">
+    <RootWrapper>
+      <Wrapper>
         <h1>{title}</h1>
         <p>{message}</p>
         <Links />
-      </div>
-    </div>
+      </Wrapper>
+    </RootWrapper>
   );
 };
 
