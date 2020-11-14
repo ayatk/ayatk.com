fe4b1c7f6429e596f263acfbe1df38a98d7d387e
ayatk <ayatk@users.noreply.github.com>
2020-11-14 20:12:33 +0900

    Cra (#250)
    * create-react-appにした
    
    * fix deploy script

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,28 +1,22 @@
-import * as React from "react";
+import React from 'react'
 
 const Links = () => {
   const links = [
-    { name: "Twitter", url: "https://twitter.com/ayatk_ch" },
-    { name: "GitHub", url: "https://github.com/ayatk" },
-    { name: "Mastodon (ぱおぱお)", url: "https://pawoo.net/@ayatk" },
-    {
-      name: "pixivFANBOX",
-      url: "https://www.pixiv.net/fanbox/creator/17461152"
-    },
-    { name: "Blog", url: "http://ayatk.hatenablog.com" }
-  ];
+    { name: 'Twitter', url: 'https://twitter.com/ayatk_ch' },
+    { name: 'GitHub', url: 'https://github.com/ayatk' },
+    { name: 'Mastodon (ぱおぱお)', url: 'https://pawoo.net/@ayatk' },
+    { name: 'Blog', url: 'http://ayatk.hatenablog.com' },
+  ]
 
   return (
     <ul>
-      {links.map(link => {
-        return (
-          <li>
-            <a href={link.url}>{link.name}</a>
-          </li>
-        );
-      })}
+      {links.map((link) => (
+        <li key={`${link.name}`}>
+          <a href={link.url}>{link.name}</a>
+        </li>
+      ))}
     </ul>
-  );
-};
+  )
+}
 
-export default Links;
+export default Links
