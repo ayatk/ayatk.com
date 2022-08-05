aaf312bb948987e0a2e7003bc6ba6714cf7d86b4
ayatk <7327867+ayatk@users.noreply.github.com>
2022-08-06 00:36:26 +0900

    Nextjs (#491)
    * use yarn v3
    
    * use next js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
   ],
   plugins: ['@typescript-eslint', 'import'],
   parser: '@typescript-eslint/parser',
